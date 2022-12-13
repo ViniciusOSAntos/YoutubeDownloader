e0e4286d51e04d124f080544ef5217ef6bc236af
Vinicius Oliveira <vine0@hotmail.com>
2022-12-13 00:08:15 -0300

    Testing itag

--- downloader.py
+++ downloader.py
@@ -7,7 +7,8 @@ print("LINK: ", link)
 
 yt.streams.filter(abr="128kbps", progressive=False, only_audio = True).first().download(filename="audio.mp3")
 #download video only
-yt.streams.filter(progressive=False).get_highest_resolution().download(filename="video.mp4")
+# yt.streams.filter(progressive=False).get_highest_resolution().download(filename="video.mp4")
+yt.streams.get_by_itag(399).download(filename="video.mp4")
 
 videoclip = VideoFileClip(r".\video.mp4")
 
@@ -16,7 +17,7 @@ audioclip = AudioFileClip("audio.mp3")
 new_audioclip = CompositeAudioClip([audioclip])
 videoclip.audio = new_audioclip
 
-videoclip.write_videofile(f"{yt.title}.mp4")
+videoclip.write_videofile(f"{yt.title}.webm")
 #videoclip.write_videofile("videoFinal.mp4")
 
 videoclip.close()
