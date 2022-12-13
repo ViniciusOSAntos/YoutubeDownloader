3ed67360d74b6cfd0a4b3f0300a89d894ab103a8
Vinicius Oliveira <vine0@hotmail.com>
2022-12-12 23:23:19 -0300

    Solving path problems

--- downloader.py
+++ downloader.py
@@ -5,19 +5,19 @@ link = str(input("Entre com o Link: "))
 yt = YouTube(link)
 print("LINK: ", link)
 
-yt.streams.filter(abr="128kbps", progressive=False, only_audio = True).first().download("audio.mp3")
+yt.streams.filter(abr="128kbps", progressive=False, only_audio = True).first().download(filename="audio.mp3")
 #download video only
-yt.streams.filter(progressive=False).get_highest_resolution().download("videoA.mp4")
+yt.streams.filter(progressive=False).get_highest_resolution().download(filename="video.mp4")
 
-videoclip = VideoFileClip("videoA.mp4")
-print("AQUI")
+videoclip = VideoFileClip(r".\video.mp4")
+
 audioclip = AudioFileClip("audio.mp3")
 
 new_audioclip = CompositeAudioClip([audioclip])
 videoclip.audio = new_audioclip
 
-# videoclip.write_videofile(f"{yt.title}.mp4")
-videoclip.write_videofile("videoFinal.mp4")
+videoclip.write_videofile(f"{yt.title}.mp4")
+#videoclip.write_videofile("videoFinal.mp4")
 
 videoclip.close()
 audioclip.close()
